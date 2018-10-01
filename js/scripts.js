70927a1004dbf03fd85c852a7db26b4e7773d9ac
Marvin Chomba <marvin.chomba24@gmail.com>
2018-10-01 18:54:28 +0300

    Add the images and html file for the homepage

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
     $("#log").show();
     $("#first").hide();
   })
+  $("#slideshow > div:gt(0)").hide();
+  setInterval(function () {
+  $('#slideshow > div:first')
+    .fadeOut(5)
+    .next()
+    .fadeIn(5)
+    .appendTo('#slideshow');
+  }, 3000)
 })
+
